74b70b132e8e68588e99f5a06ff7da626c06e075
bo <wb960228@163.com>
2022-10-10 18:38:23 +0800

    初始化提交10.10

--- gmall-admin/src/test/java/atguigu/BoTest.java
+++ gmall-admin/src/test/java/atguigu/BoTest.java
@@ -3,20 +3,14 @@ package atguigu;
 import org.junit.Test;
 import org.springframework.boot.SpringBootVersion;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class BoTest {
     @Test
     public void test() {
-/*        String a = SpringBootVersion.getVersion();
-        System.out.println(a);
+
         int arr[][] = new int[][]{{1, 2, 3}, {2}, {3}, {3, 4}};
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.println(arr[i][j] + "\t");
-            }
-        }*/
-        int arr[][] = new int[][]{{1, 2, 3}, {2}, {3}, {3, 4}};
-        for (int j = 0; j < arr[0].length; j++) {
-            System.out.println(arr[0][j] + "\t");
-        }
+            System.out.println(Arrays.toString(arr[0]));
     }
 }
